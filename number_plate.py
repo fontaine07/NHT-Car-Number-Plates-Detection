866a67b6634f7f1884bb42fd3c88f46a43959cf5
fontaine07 <tramnn.0702@gmail.com>
2023-07-28 16:02:49 +0700

    first commit

---
+++ number_plate.py
@@ -0,0 +1,54 @@
+import cv2
+import easyocr
+
+harcascade = "model/haarcascade_russian_plate_number.xml"
+
+# Initialize the easyOCR reader with the desired language
+reader = easyocr.Reader(['en'])
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 640) # width
+cap.set(4, 480) #height
+
+min_area = 500
+count = 0
+
+
+while True:
+    success, img = cap.read()
+
+    plate_cascade = cv2.CascadeClassifier(harcascade)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
+
+    for (x,y,w,h) in plates:
+        area = w * h
+
+        if area > min_area:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+            cv2.putText(img, "Number Plate", (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
+
+            img_roi = img[y: y+h, x: x+w]
+            cv2.imshow("ROI", img_roi)
+
+            # Use easyOCR to recognize text on the number plate
+            result = reader.readtext(img_roi)
+            
+            # Print the recognized text to the terminal
+            if len(result) > 0:
+                text = result[0][1]
+                print("Detected Text:", text)
+    
+    cv2.imshow("Result", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("plates/scaned_img_" + str(count) + ".jpg", img_roi)
+        cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
+        cv2.putText(img, "Scan Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255), 2)
+        cv2.imshow("Results",img)
+        cv2.waitKey(500)
+        count += 1
+        
+
